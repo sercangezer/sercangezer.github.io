76faca62067a0e5e1577bf2bfda0c58540f6fb5e
Sercan GEZER <sercan.gezer.TR@gmail.com>
2023-10-20 20:07:09 +0300

    tags folder create

---
+++ assets/tags-creator.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+tags_array=("AWX" "AWX Credential Type" "Analyzing Container Behavior with Falco" "Ansible" "CKS" "CPU Limits" "CPU Request" "CVE" "Centos 7" "Container" "Continious Delivery" "Continious Integration" "DevOps" "Disk Yönetimi" "Dosya Sistemi" "Freezing" "GIT" "Github" "Github API" "Github Organizasyon" "Github Repo" "Jekyll" "Jenkins" "K8s" "Kernel" "Kubernetes" "Kubernetes tool" "Kubeview" "Lens" "Lens resource map eklentisi" "Linux" "Memory Limits" "Memory Request" "Nexus Repository" "Pod" "Policy" "RAID" "Redhat based" "Ruby" "SELinux" "SELinux disable" "Source Control" "Vagrant" "Vagrant Provider" "VirtualBox" "allowPrivilegeEscalation" "auditing" "container" "container security context" "dockerfile" "falco" "gvisor" "güvenlik açığı" "image" "imaj" "immutable" "immutable container" "immutable pod" "kubeapiserver" "kubelet" "logging" "minTtlsMinVersion" "pod" "privileged" "rbenv" "readOnlyRootFilesystem" "runc" "runsc" "runtimeclass" "tlsciphersuites" "tlsminversion" "tlsCipherSuites" "trivy")
+
+rm -rf /e/Git-repositories/sercangezer.github.io/tag/*
+
+for i in "${tags_array[@]}"
+do
+
+cd /e/Git-repositories/sercangezer.github.io/tag
+ mkdir "$i"
+cd "/e/Git-repositories/sercangezer.github.io/tag/$i"
+
+cat << EOF > index.html
+---
+layout: tag
+tag: $i
+---
+EOF
+done
